1119bead9016f4f29eef5f88b5e0dbf06598b667
sayedfarhad <sayedfarhad@sayeds-mbp-2.hsd1.ca.comcast.net>
2023-01-12 20:37:04 -0800

    I have added my Boolean SleepIn Class,Farhad

---
+++ src/test/java/com/gitHub/teamD/FarhadsClass.java
@@ -0,0 +1,31 @@
+package com.gitHub.teamD;
+
+public class FarhadsClass {
+	
+	
+	public static void main(String[] args) {
+
+		boolean r1 = SleepIn(false, false);
+		boolean r2 = SleepIn(true, false);
+
+		System.out.println(r1);
+		System.out.println(r2);
+
+	}
+
+	public static boolean SleepIn(boolean isWeekday, boolean isVacation) {
+
+		boolean result = true;
+
+		if (! isWeekday  || isVacation ) {
+			result = false;
+
+		}
+
+		return result;
+
+	}
+
+	
+
+}
