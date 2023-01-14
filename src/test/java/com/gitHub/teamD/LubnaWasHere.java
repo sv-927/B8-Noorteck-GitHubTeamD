afa782a0607130be3a9681ef0108632af8ac355c
lublu <lublu@LubnaAlshakoor.myfiosgateway.com>
2023-01-13 23:23:30 -0500

    First commit

---
+++ src/test/java/com/gitHub/teamD/LubnaWasHere.java
@@ -0,0 +1,12 @@
+package com.gitHub.teamD;
+
+public class LubnaWasHere {
+	
+	public static void main(String[] args) {
+		
+		String a = "lubna";
+		
+		System.out.println(a.toUpperCase());
+	}
+
+}
