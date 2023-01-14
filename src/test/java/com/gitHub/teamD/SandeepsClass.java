e32340e505fc66a2a8f9ab199c9365aa27b83766
ALIALKHAFAJ <121980746+ALIALKHAFAJ@users.noreply.github.com>
2023-01-14 03:39:52 -0500

    Ali did some change

--- src/test/java/com/gitHub/teamD/SandeepsClass.java
+++ src/test/java/com/gitHub/teamD/SandeepsClass.java
@@ -37,5 +37,16 @@ public class SandeepsClass extends LubnaWasHere {
 
 		return a + b;
 	}
+	public static boolean isPrime(int n) {
+	    if (n <= 1) {
+	        return false;
+	    }
+	    for (int i = 2; i < n; i++) {
+	        if (n % i == 0) {
+	            return false;
+	        }
+	    }
+	    return true;
+	}
 
 }
